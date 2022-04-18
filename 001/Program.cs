1431bac8f5a5704107a3bfb3c076869e5468453d
«Vova <vrobskiy@gmail.com>
2022-04-19 01:37:47 +0300

    add Example 001

---
+++ 001/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (x == 0 || y == 0)
+{
+    Console.WriteLine("X или Y не могуит бьть равны 0");
+    return;
+}
+int quarterNumber = 1;
+
+if (x < 0 && y > 0)
+{
+    quarterNumber = 2;
+}
+if (x < 0 && y < 0)
+{
+    quarterNumber = 3;
+}
+if (x > 0 && y < 0)
+{
+    quarterNumber = 4;
+}
+
+Console.WriteLine($"Точка находится в четверти номер {quarterNumber}");
+
+
---
+++ 002/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int quarterNumber = Convert.ToInt32(Console.ReadLine());
+
+Quarter(quarterNumber);
+
+void Quarter(int quarterNumber)
+{
+if (quarterNumber == 1)
+    Console.WriteLine($"Х: (+1;{Int64.MaxValue}), а Y: (1;{Int64.MaxValue})");
+if (quarterNumber == 2)
+    Console.WriteLine($"Х: (-1;{Int64.MinValue}), а Y: (1;{Int64.MaxValue})");
+if (quarterNumber == 3)
+    Console.WriteLine($"X: (-1;{Int64.MinValue}), Y: (-1;{Int64.MinValue})");
+if (quarterNumber == 4)
+    Console.WriteLine($"X: (1;{Int64.MaxValue}), Y: (-1;{Int64.MinValue})");
+if (quarterNumber <1 || quarterNumber > 4)
+{
+    Console.WriteLine($"Введите корректный номер четверти!!!!");
+    return;
+}
+}
